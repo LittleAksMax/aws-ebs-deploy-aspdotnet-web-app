2f5610de5e04718dda26240ed642e13eac8528be
David <david2005.rosental@gmail.com>
2023-07-30 15:32:33 +0100

    Made sure that TodoId on the Todo domain entity has a primary key index

--- App.Domain/Models/Todo.cs
+++ App.Domain/Models/Todo.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 namespace App.Domain.Models;
 
 [Index(nameof(Title), IsUnique = true)] // add UNIQUE constraint to Title property
+[PrimaryKey(nameof(TodoId))]
 public sealed class Todo
 {
     // TodoId not required since it can't necessarily be instantiated
